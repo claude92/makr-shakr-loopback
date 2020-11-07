19f4ddef371f8aa14042b6a98440f23f4d644030
claudiuPunch <67048726+claudiuPunch@users.noreply.github.com>
2020-11-07 19:33:43 +0100

    register api, login, authenticated requests

---
+++ server/boot/role-resolver.js
@@ -0,0 +1,20 @@
+'use strict';
+const jwt = require('jsonwebtoken');
+
+module.exports = function(app) {
+  var Role = app.models.Role;
+  Role.registerResolver('JWTauthenticated', (role, context, cb) => {
+    let accessToken = context.remotingContext.req.cookies.jwt;
+    if (!accessToken) {
+      return process.nextTick(() => cb(null, false));
+    }
+    let payload;
+    try {
+      console.log(process.env.ACCESS_TOKEN_SECRET);
+      payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+    } catch (e) {
+      return process.nextTick(() => cb(null, false));
+    }
+    return process.nextTick(() => cb(null, true));
+  });
+};
---
+++ server/controllers/users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const jwt = require('jsonwebtoken');
+const {User} = require('loopback');
+
+module.exports = {
+  login: (req, res) => {
+    const {email, password} = req.body;
+    console.log(email);
+    User.findOne({email}, (err, user) => {
+      let result = {};
+      let status = 200;
+      if (!err && user) {
+        user.hasPassword(password, (err, isMatch) => {
+                    // password matched
+          if (!err && isMatch === true) {
+            status = 200;
+            const payload = {user: user.email};
+            let accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {algorithm: 'HS256', expiresIn: process.env.ACCESS_TOKEN_LIFE});
+            let refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {algorithm: 'HS256', expiresIn: process.env.REFRESH_TOKEN_LIFE});
+            res.cookie('jwt', accessToken, {httpOnly: true});
+            res.status(status).send({login:accessToken });
+          } else {
+                        // unauthorized
+            res.status(401).send('WRONG_PASSWORD');
+          }
+        });
+      } else {
+        console.log('user not found');
+                // unauthorized
+        res.status(401).send();
+      }
+    });
+  },
+};
